0b0ef05836a6d804f0537dd41e965f0df5558e1d
alimagdi12 <alimagdi12367@yahoo.com>
2024-06-17 08:13:36 +0300

    notification commit in the frontend and solving some problems

--- final-project/React-Final-Project/src/contexts/NotificationContext.jsx
+++ final-project/React-Final-Project/src/contexts/NotificationContext.jsx
@@ -1,18 +1,13 @@
 import React, { createContext, useState, useEffect } from 'react';
 
-// Create the NotificationContext
 const NotificationContext = createContext();
 
-// NotificationProvider component to wrap around parts of the app that need access to the notifications
 const NotificationProvider = ({ children }) => {
   const [notifications, setNotifications] = useState([]);
 
-  // Function to fetch notifications
-  // Function to fetch notifications
-// Function to fetch notifications
+
 const fetchNotifications = async () => {
     try {
-      // Retrieve token from local storage  
       const response = await fetch('http://127.0.0.1:3000/api/v1/auth/get-notification', {
         headers: {
             'Content-Type': 'multipart/form-data',
@@ -33,7 +28,6 @@ const fetchNotifications = async () => {
   };
     
 
-  // Use useEffect to fetch notifications when the component mounts
   useEffect(() => {
     fetchNotifications();
   }, []);
