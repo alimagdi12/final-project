d1608be623ce30ad9910cba5603c4389db32dc81
OmarGTolba <omar.739.tolba@gmail.com>
2024-06-29 05:10:45 +0300

    home navigations

--- final-project/React-Final-Project/src/pages/Home/components/CategoriesSection.jsx
+++ final-project/React-Final-Project/src/pages/Home/components/CategoriesSection.jsx
@@ -3,10 +3,11 @@ import { Box, Container, Grid } from '@mui/material';
 import CardHeader from './CardHeader.jsx';
 import AboutCard from '../../About/Components/AboutCard.jsx';
 import CategoryContext from '../../../contexts/CategoriesContext.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const CategoriesSection = () => {
   const { categories } = useContext(CategoryContext);
-
+  const navigate = useNavigate()
   if (!categories.categories) {
     return null;
   }
@@ -16,7 +17,7 @@ const CategoriesSection = () => {
       <CardHeader>Categories</CardHeader>
       <Grid container spacing={2}>
         {categories.categories.slice(0, 4).map((category, index) => (
-          <Grid item xs={12} sm={6} md={3} key={index} sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Grid item xs={12} sm={6} md={3} key={index} sx={{ display: 'flex', justifyContent: 'center' }} onClick={()=>{navigate(`/products/${category._id}`)}} >
             <AboutCard img={category?.imageUrl?.images[0]} name={category.title}>
               {category.title}
             </AboutCard>
--- final-project/React-Final-Project/src/pages/Home/components/ProductsSection.jsx
+++ final-project/React-Final-Project/src/pages/Home/components/ProductsSection.jsx
@@ -3,10 +3,11 @@ import { Container, Grid } from '@mui/material';
 import CardHeader from './CardHeader.jsx';
 import MainCard from './MainCard.jsx';
 import ProductsContext from '../../../contexts/ProductsContext.jsx';
+import { useNavigate } from 'react-router-dom';
 
 const ProductsSection = () => {
   const { products } = useContext(ProductsContext);
-
+const navigate = useNavigate()
   if (!products.products) {
     return null;
   }
@@ -16,7 +17,7 @@ const ProductsSection = () => {
       <CardHeader>Products</CardHeader>
       <Grid container spacing={2}>
         {products.products.slice(0, 4).map((product, index) => (
-          <Grid item xs={12} sm={6} md={3} key={index}>
+          <Grid item xs={12} sm={6} md={3} key={index} onClick={()=>{navigate(`/product-details/${product._id}`)}} >
             <MainCard product={product} />
           </Grid>
         ))}
